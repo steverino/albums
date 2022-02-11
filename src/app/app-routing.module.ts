0b779ad44e95de513aeb1ddc395b3acb85e0ee0a
Steve <stvreeno_3@yahoo.com>
2022-02-11 07:44:21 -0500

    songs init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ArtistsComponent } from './artists/artists.component';
 import { AlbumsComponent } from './albums/albums.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SongsComponent } from './songs/songs.component';
 
 const routes: Routes = [
   
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'albums', component: AlbumsComponent },
-  { path: 'artists', component: ArtistsComponent }
+  { path: 'artists', component: ArtistsComponent },
+  { path: 'songs', component: SongsComponent }
 ];
 
 @NgModule({
